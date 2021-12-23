72a00fb04a30691dbfec24db15d419a22a317b38
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2021-12-23 20:49:21 +0100

    Add basic command line support

--- cmd/mtga/main.go
+++ cmd/mtga/main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
 	"github.com/maitesin/mtga/internal/app"
-	sql2 "github.com/maitesin/mtga/internal/infra/sql"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/maitesin/mtga/internal/infra/cmd"
+	sqlx "github.com/maitesin/mtga/internal/infra/sql"
 	"github.com/upper/db/v4/adapter/sqlite"
 )
 
 func main() {
-	db, err := sql.Open("sqlite", "./cards.db")
+	var settings = sqlite.ConnectionURL{
+		Database: `cards.db`, // Path to database file
+	}
+
+	// Attempt to open the 'example.db' database file
+	sess, err := sqlite.Open(settings)
 	if err != nil {
 		panic(err)
 	}
 
-	dbConn, err := sqlite.New(db)
-	if err != nil {
-		panic(err)
-	}
-
-	repository := sql2.NewCardsRepository(dbConn)
+	repository := sqlx.NewCardsRepository(sess)
 
 	addCommandHandler := app.NewCreateCardHandler(repository)
 
 	_ = addCommandHandler
+
+	var opts cmd.Options
+
+	var parser = flags.NewParser(&opts, flags.Default)
+
+	if values, err := parser.Parse(); err != nil {
+		switch flagsErr := err.(type) {
+		case flags.ErrorType:
+			if flagsErr == flags.ErrHelp {
+				os.Exit(0)
+			}
+			os.Exit(1)
+		default:
+			fmt.Printf("Values %+v\n", values)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/mattn/go-sqlite3 v1.14.7
 	github.com/stretchr/testify v1.7.0
 	github.com/upper/db/v4 v4.2.1
@@ -11,12 +12,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/matryer/moq v0.2.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/quasilyte/go-ruleguard/dsl v0.3.10 // indirect
-	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/tools v0.1.8 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
