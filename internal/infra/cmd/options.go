852dc3c46120f81d122047ee7628bfb6e9744e10
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-01-05 22:26:54 +0100

    Remove the default values of the boolean cmd options

--- internal/infra/cmd/options.go
+++ internal/infra/cmd/options.go
@@ -4,8 +4,8 @@ type Options struct {
 	Number    int    `short:"n" long:"number" description:"Number of the card in the set"`
 	Set       string `short:"s" long:"set" description:"Magic the Gathering set of the card"`
 	Quantity  int    `short:"q" long:"quantity" default:"1" description:"Number of copies of that card"`
-	Foil      bool   `short:"f" long:"foil" default:"false" description:"Foil card"`
-	Altered   bool   `short:"a" long:"altered" default:"false" description:"Altered card"`
-	Signed    bool   `short:"i" long:"signed" default:"false" description:"Signed card"`
+	Foil      bool   `short:"f" long:"foil" description:"Foil card"`
+	Altered   bool   `short:"a" long:"altered" description:"Altered card"`
+	Signed    bool   `short:"i" long:"signed" description:"Signed card"`
 	Condition string `short:"c" long:"condition" default:"nm" description:"Card's condition'"`
 }
